3c14e74d2e19ce02b2740385be8cdb2aeb27f227
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-22 00:36:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str) {
+   
+    return str.replace(/[^a-zA-Z0-9-' '']/g, '');
+   }
+  static titleize(str) {
+     
+    return str.replace(/[^a-zA-Z0-9-' '']/g, ' ').split(' ').map(word => {
+      if(word=='and' || word=='the' || word=='of' || word=='a' || word=='an' || word=='but' || word=='for' || word=='at' || word=='by' || word=='from'){
+        return word;
+      }else{
+        
+        return this.capitalize(word);
+      }
+    }).join(' ');
+  }
+
 }
